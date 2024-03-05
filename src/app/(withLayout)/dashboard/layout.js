95ad72c4f1ae6bae80d9b69e643e6c455ebbb312
Kakon Debnath <kakondebnath96@gmail.com>
2024-03-05 19:35:57 +0600

    group Routing

---
+++ src/app/(withLayout)/dashboard/layout.js
@@ -0,0 +1,8 @@
+import Footer from "@/components/shared/Footer/Footer";
+
+const DashboardLayout = ({ children }) => {
+  return <div>{children}
+  <Footer />
+  </div>;
+};
+export default DashboardLayout;
---
+++ src/app/(withLayout)/dashboard/user/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserPage = () => {
+  return (
+    <div>
+      <h1>User Page Of Dashboard Route</h1>
+    </div>
+  );
+};
+
+export default UserPage;
